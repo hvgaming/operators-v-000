789aea329ec53d78df98f93310a40f785f425eb2
hugo valois <hvgaming@yahoo.com>
2018-08-27 20:17:08 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,7 +10,7 @@ end
 
 
 def not_safe?(speed)
-speed > 60 ? speed < 40 ? speed > 40 && speed < 60 : 	
+speed > 60 ? return true : speed < 40 ?  return true : speed > 40 && speed < 60 ? return false : 	
 end
 	
 
